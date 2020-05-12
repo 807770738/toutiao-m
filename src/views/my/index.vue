aeeeef231bc6213162d35ebd79c3aeaa753d76e4
wang-qi6666 <807770738@qq.com>
2020-05-13 00:10:37 +0800

    完成首页基本功能

--- src/views/my/index.vue
+++ src/views/my/index.vue
@@ -1,34 +1,40 @@
 <template>
   <div class="my-Container">
     <!-- 头部 -->
-    <van-cell-group  class="header">
+    <van-cell-group v-if="user" class="header">
       <van-cell title="单元格" value="内容" center :border="false">
         <div slot="icon">
-          <van-image width="50" height="50" round fit="cover" src="https://img.yzcdn.cn/vant/cat.jpeg" />
+          <van-image width="50" height="50" round fit="cover" :src="currentUser.photo" />
         </div>
-        <div slot="title" class="title">黑马头条号</div>
+        <div slot="title" class="title">{{currentUser.name}}</div>
         <van-button size="mini" round class="btn">编辑资料</van-button>
       </van-cell>
       <!-- 导航栏 -->
          <van-grid :border="false" class="nav">
         <van-grid-item>
-         <div>111</div>
+         <div>{{currentUser.art_count}}</div>
          <div class="text">头条</div>
         </van-grid-item>
         <van-grid-item >
-         <div>111</div>
+         <div>{{currentUser.follow_count}}</div>
          <div class="text">关注</div>
         </van-grid-item>
         <van-grid-item>
-         <div>111</div>
+         <div>{{currentUser.fans_count}}</div>
          <div class="text">粉丝</div>
         </van-grid-item>
         <van-grid-item>
-         <div>111</div>
+         <div>{{currentUser.like_count}}</div>
          <div class="text">获赞</div>
         </van-grid-item>
       </van-grid>
     </van-cell-group>
+    <div v-else class="no-login">
+      <div class="img" @click="$router.push('/login')">
+        <img src="./头像.png" alt="">
+      </div>
+      登录 / 注册
+    </div>
     <!-- 内容区 -->
     <div>
     <van-grid :column-num="2" class="tubiao">
@@ -43,23 +49,49 @@
     </div>
 
     <!-- 退出登录 -->
-    <van-button class="tuichu" block>退出登录</van-button>
+    <van-button class="tuichu" block v-if="user" @click="onlogin">退出登录</van-button>
   </div>
 </template>
 
 <script>
+import { mapState } from 'vuex'
+import { GetUser } from '@/api/user'
 export default {
   name: 'myIndex',
   components: {},
   props: {},
   data () {
-    return {}
+    return {
+      currentUser: {}
+    }
   },
-  computed: {},
+  computed: {
+    ...mapState(['user'])
+  },
   watch: {},
-  created () {},
+  created () {
+    this.loadcrentUser()
+  },
   mounted () {},
-  methods: {}
+  methods: {
+    async loadcrentUser () {
+      const { data } = await GetUser()
+      this.currentUser = data.data
+    },
+    onlogin () {
+      this.$dialog.confirm({
+        title: '标题',
+        message: '弹窗内容'
+      })
+        .then(() => {
+          // on confirm
+          this.$store.commit('setUser', null)
+        })
+        .catch(() => {
+          // on cancel
+        })
+    }
+  }
 }
 </script>
 
@@ -100,5 +132,23 @@ export default {
 .tubiao{
   color:#d86262;
   background-color: #fff;
+}
+.no-login{
+  height: 180px;
+  background: url(./banner.png) no-repeat;
+  background-size: cover;
+  color: #fff;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  font-size: 14px;
+  .img{
+    padding-bottom: 5px;
+  }
+  img{
+    width: 66px;
+    height: 66px;
+  }
 }
 </style>
